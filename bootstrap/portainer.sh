c504ee964d8f600b4e42c45465e29eb93602edba
iLTeoooD <matteopietro.dazzi@gmail.com>
2020-01-04 00:00:46 +0100

    Add portainer at boot

---
+++ bootstrap/portainer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -xe
+
+cat <<EOF > /usr/bin/portainer
+#!/bin/sh
+service docker start
+docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+EOF
+
+cat <<EOF > /etc/init.d/portainer
+!/sbin/openrc-run
+command="/usr/bin/portainer"
+command_background=false
+depend() {
+  need docker
+}
+EOF
+
+chmod +x /etc/init.d/portainer /usr/bin/portainer
+rc-update add portainer
