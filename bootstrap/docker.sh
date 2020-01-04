5b829ecfc06af02cf9baf00c641e6902d5e8c092
Matteo Pietro Dazzi <matteopietro.dazzi@gft.com>
2020-01-04 21:39:08 +0100

    Removed docker-compose

--- bootstrap/docker.sh
+++ bootstrap/docker.sh
@@ -4,8 +4,3 @@ set -xe
 
 #insall docker
 apk add docker
-
-#install docker-compose
-apk add py-pip
-apk add python-dev libffi-dev openssl-dev gcc libc-dev make
-pip install --no-cache-dir docker-compose
