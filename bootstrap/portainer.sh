c2530a3f0455bdc02885cde0928e7a28a44f28fd
Matteo Pietro Dazzi <matteopietro.dazzi@gft.com>
2020-01-04 21:07:28 +0100

    Portainer start with crontab

--- bootstrap/portainer.sh
+++ bootstrap/portainer.sh
@@ -2,20 +2,6 @@
 
 set -xe
 
-cat <<EOF > /usr/bin/portainer
-#!/bin/sh
-docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+cat <<EOF > /etc/crontabs/pi
+@reboot sleep 10 && docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --name portainer --restart always portainer/portainer
 EOF
-
-cat <<EOF > /etc/init.d/portainer
-#!/sbin/openrc-run
-command="/usr/bin/portainer"
-command_background=false
-depend() {
-  after sshd
-  need docker
-}
-EOF
-
-chmod +x /etc/init.d/portainer /usr/bin/portainer
-rc-update add portainer default
--- bootstrap/services.sh
+++ bootstrap/services.sh
@@ -8,11 +8,11 @@ for service in devfs dmesg mdev; do
 	rc-update add $service sysinit
 done
 
-for service in modules sysctl hostname bootmisc swclock syslog swap docker; do
+for service in modules sysctl hostname bootmisc swclock syslog swap; do
 	rc-update add $service boot
 done
 
-for service in dbus haveged sshd chronyd local wpa_supplicant networking avahi-daemon bluetooth; do
+for service in dbus haveged sshd chronyd local wpa_supplicant networking avahi-daemon bluetooth docker crond; do
 	rc-update add $service default
 done
 
