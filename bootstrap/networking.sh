ac6b75339dddd77a3dd09c50927f07b64f27de9c
lguzzon <luca.guzzon@gmail.com>
2020-01-08 11:25:56 -0500

    Little silly fixes ...

--- bootstrap/networking.sh
+++ bootstrap/networking.sh
@@ -5,9 +5,9 @@ set -xe
 apk add wpa_supplicant wireless-tools wireless-regdb iw
 sed -i 's/wpa_supplicant_args=\"/wpa_supplicant_args=\" -u -Dwext,nl80211/' /etc/conf.d/wpa_supplicant
 
-echo -e 'brcmfmac' >> /etc/modules
+echo -e 'brcmfmac' >>/etc/modules
 
-cat <<EOF > /boot/wpa_supplicant.conf
+cat <<EOF >/boot/wpa_supplicant.conf
 network={
  ssid="SSID"
  psk="PASSWORD"
@@ -16,7 +16,7 @@ EOF
 
 ln -s /boot/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
-cat <<EOF > /etc/network/interfaces
+cat <<EOF >/etc/network/interfaces
 auto lo
 iface lo inet loopback
 
