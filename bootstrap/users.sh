dde69bdb323f7f9ceb73346a17454f47599d4a17
Kleo Bercero <kbeflo@gmail.com>
2021-06-28 07:05:24 +0800

    Remove additional users

--- bootstrap/users.sh
+++ bootstrap/users.sh
@@ -2,19 +2,6 @@
 
 set -xe
 
-FIRST_USER_NAME=pi
-
-apk add sudo
-
-for GRP in spi i2c gpio docker; do
-  addgroup --system $GRP
+for GRP in spi i2c gpio; do
+	addgroup --system $GRP
 done
-
-adduser -s /bin/bash -D $FIRST_USER_NAME
-
-for GRP in adm dialout cdrom audio users video games input gpio spi i2c netdev docker; do
-  adduser $FIRST_USER_NAME $GRP
-done
-
-echo "pi:raspberry" | /usr/sbin/chpasswd
-echo "pi ALL=NOPASSWD: ALL" >>/etc/sudoers
