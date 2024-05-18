87055a34372d67ff74a924a5b56c02099c6f8758
kleo <kleo@hak.dog>
2024-05-18 21:44:25 +0800

    remove exit 0 if hashes match

--- bootstrap/fusee.sh
+++ bootstrap/fusee.sh
@@ -12,7 +12,6 @@ FUSEE_LAUNCHER_CALCULATED_HASH=$(sha256sum /etc/fusee-launcher/1.0.zip | awk '{p
 
 if [ "$FUSEE_LAUNCHER_CALCULATED_HASH" == "$FUSEE_LAUNCHER_HASH" ]; then
     echo "Hashes match. File integrity verified."
-    exit 0
 else
     echo "Hashes do not match. File may be corrupted."
     exit 1
